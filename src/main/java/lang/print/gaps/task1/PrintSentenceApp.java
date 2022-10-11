89743040e848ffaf9d1ab04c2342a874acc3eb79
Akobir Abduganiev <akobir.abduganiev@ya.ru>
2022-10-11 11:35:38 +0500

    success

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
+        //write your code here
+        System.out.println("This is my first Java program");
     }
 }
